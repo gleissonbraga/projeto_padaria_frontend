c1af6d174b36a62c996b6d1b47ec5e190af8675c
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2025-09-24 17:06:11 -0300

    feat: inserindo sections descricao

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import Home from "./Components/home/Home";
-import Descricao from "./Components/images/Descricao";
-import NavBar from "./Components/navbar/navbar";
+import Descricao from "./Components/images/Descricao.jsx";
+import NavBar from "./Components/navbar/NavBar.jsx";
 import "./index.css";
-import Footer from "./Components/footer/footer";
+import Footer from "./Components/footer/footer.jsx";
 function App() {
   return (
     <div className="App">

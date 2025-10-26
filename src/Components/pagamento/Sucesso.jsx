a1886f3bd30b18f87551882fb505881b3d1a06a4
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2025-10-26 12:03:12 -0300

    doc: descomentando endpoint de status pedido

--- src/Components/pagamento/Sucesso.jsx
+++ src/Components/pagamento/Sucesso.jsx
@@ -8,13 +8,14 @@ export default function Sucesso(){
     const paymentId = query.get("payment_id");
     const status = query.get("status");
     const preferenceId = query.get("preference_id");
-console.log(paymentId)
+
+    //https://projeto-padaria-frontend.vercel.app/pagamento/sucesso?collection_id=131360417694&collection_status=approved&payment_id=131360417694&status=approved&external_reference=null&payment_type=credit_card&merchant_order_id=35040995839&preference_id=2694156643-48b8bafd-31d4-4ff5-9ade-329c822550e5&site_id=MLB&processing_mode=aggregator&merchant_account_id=null
 
   useEffect(() => {
     const fetchData = async () => {
         if (status === "approved" && preferenceId) {
             try {
-                // await apiClient.post(`pagamento/sucesso/${preferenceId}/${paymentId}`);
+                await apiClient.post(`pagamento/sucesso/${preferenceId}/${paymentId}`);
                 const responsePedido = await apiClient.get(`pedidos/aprovado/${preferenceId}`);
                 setPedido(responsePedido.data)
             } catch (error) {

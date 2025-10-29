388ca31319d107df762e2271e08e5c15088baa13
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2025-10-29 18:20:06 -0300

    fix: ajustando mobile

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -2,7 +2,7 @@ import Carrossel from "./components/Carrossel";
 
 export default function Home(){
     return (
-        <section id="home" className="w-full min-[430px]:h-[600px] h-screen bg-[url('/images/bg-home-2.png')] bg-cover bg-center pt-40 flex justify-center min-[430px]:p-0 pl-28 items-end">
+        <section id="home" className="w-full min-[430px]:h-[600px] sm:h-screen bg-[url('/images/bg-home-2.png')] bg-cover bg-center pt-40 flex justify-center min-[430px]:p-0 sm:pl-28 items-end">
             <div className="w-[70%] flex text-center">
                 <div className="w-full flex justify-center">
                     {/* <div className="font-sansita text-white text-7xl flex flex-col text-start pt-6">

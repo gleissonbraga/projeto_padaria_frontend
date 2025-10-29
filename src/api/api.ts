9f38309116684a1b29905b2ee8bdb849ab21a32d
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2025-10-29 18:20:50 -0300

    fix: ajustando mobile

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,29 +2,29 @@ import axios from 'axios';
 import type { AxiosInstance} from 'axios';
 
 const apiClient: AxiosInstance = axios.create({
-    // baseURL: "https://padaria-api-sui1.onrender.com/api/",
-    baseURL: "http://localhost:5000/api/",
+    baseURL: "https://padaria-api-sui1.onrender.com/api/",
+    // baseURL: "http://localhost:5000/api/",
     headers: {'content-type': 'application/json'},
     timeout: 1000000,
 })
-
-export const uploadImage = async (file: File) => {
-  const formData = new FormData();
-  formData.append("file", file);
-
-  return await axios.post("http://localhost:5000/api/produtos/upload", formData, {
-    headers: { "Content-Type": "multipart/form-data" },
-  });
-};
 
 // export const uploadImage = async (file: File) => {
 //   const formData = new FormData();
 //   formData.append("file", file);
 
-//   return await axios.post("https://padaria-api-sui1.onrender.com/api/produtos/upload", formData, {
+//   return await axios.post("http://localhost:5000/api/produtos/upload", formData, {
 //     headers: { "Content-Type": "multipart/form-data" },
 //   });
 // };
+
+export const uploadImage = async (file: File) => {
+  const formData = new FormData();
+  formData.append("file", file);
+
+  return await axios.post("https://padaria-api-sui1.onrender.com/api/produtos/upload", formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+};
 
 apiClient.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
